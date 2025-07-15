5000fce857b886a0639310601b453e147dfa454b
karasmilad <elpopkaras12@gmail.com>
2025-07-15 23:40:07 +0300

    Question20

--- AssignmentC#05/Program.cs
+++ AssignmentC#05/Program.cs
@@ -6,23 +6,46 @@
         {
             #region Question019
 
+            //int size;
+            //do
+            //{
+            //    Console.Write("Enter the size of the identity matrix : ");
+            //}
+            //while (!int.TryParse(Console.ReadLine(), out size));
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        if (i == j)
+            //            Console.Write("1 ");
+            //        else
+            //            Console.Write("0 ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region Question20
             int size;
             do
             {
-                Console.Write("Enter the size of the identity matrix : ");
+                Console.Write("Enter The Size of the Array: ");
             }
             while (!int.TryParse(Console.ReadLine(), out size));
+            int[] arr = new int[size];
             for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < size; j++)
+                do
                 {
-                    if (i == j)
-                        Console.Write("1 ");
-                    else
-                        Console.Write("0 ");
+                    Console.Write($"Enter the Element {i + 1} : ");
                 }
-                Console.WriteLine();
+                while (!int.TryParse(Console.ReadLine(), out arr[i]));
             }
+            int sum = 0;
+            for (int j = 0; j < size; j++)
+            {
+                sum += arr[j];
+            }
+            Console.WriteLine($"The Sum of the Array is : {sum}");
             #endregion
         }
     }
