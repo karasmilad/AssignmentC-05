c2a6428164fab29523234fffa5cb328828beea78
karasmilad <elpopkaras12@gmail.com>
2025-07-15 23:27:43 +0300

    Question19

--- AssignmentC#05/Program.cs
+++ AssignmentC#05/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
+            #region Question019
 
+            int size;
+            do
+            {
+                Console.Write("Enter the size of the identity matrix : ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out size));
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j)
+                        Console.Write("1 ");
+                    else
+                        Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+            #endregion
         }
     }
 }
